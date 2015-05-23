7257a7b25f5cdaf9439890168f86fa53a9513c7b
Dan Asselin <dasselin86@gmail.com>
2015-05-23 16:47:08 -0400

    Add all artist actions

--- iradonrails/app/controllers/artists_controller.rb
+++ iradonrails/app/controllers/artists_controller.rb
@@ -1,2 +1,21 @@
 class ArtistsController < ApplicationController
+  def create
+    #POST create a new artist
+  end
+
+  def new
+    #GET a new artist form for user
+  end
+
+  def edit
+    #GET an artist edit form for user
+  end
+
+  def update
+    #POST edits to db
+  end
+
+  def destroy
+    #delete artist record
+  end
 end
