33f872d0f4ba41c0a8025c65d5034938aa853043
Dan Asselin <dasselin86@gmail.com>
2015-05-24 19:31:42 -0400

    Change data-type for founding_date attr in record_label_migration
    Using the date object here creates unnecessary complications. Refactored data-type to string

--- iradonrails/db/migrate/20150524183936_create_record_labels.rb
+++ iradonrails/db/migrate/20150524183936_create_record_labels.rb
@@ -2,7 +2,7 @@ class CreateRecordLabels < ActiveRecord::Migration
   def change
     create_table :record_labels do |t|
       t.string :name, null: false
-      t.date :founding_date
+      t.string :founding_date
 
       t.timestamps null: false
     end
