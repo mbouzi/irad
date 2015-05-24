050d70e30d05db05cf9f60fd292e63e42991abb8
Dan Asselin <dasselin86@gmail.com>
2015-05-24 15:08:17 -0400

    Create record_labels controller with all CRUD actions

---
+++ iradonrails/app/controllers/record_labels_controller.rb
@@ -0,0 +1,27 @@
+class ArtistsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def record_label_params
+  end
+
+end
