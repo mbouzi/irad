8a392c0c8c4afa92b37d8bb00393e1e25c0cfad6
Dan Asselin <dasselin86@gmail.com>
2015-05-24 14:55:35 -0400

    Add create_record_labels migration

---
+++ iradonrails/db/migrate/20150524183936_create_record_labels.rb
@@ -0,0 +1,10 @@
+class CreateRecordLabels < ActiveRecord::Migration
+  def change
+    create_table :record_labels do |t|
+      t.string :name, null: false
+      t.date :founding_date
+
+      t.timestamps null: false
+    end
+  end
+end
