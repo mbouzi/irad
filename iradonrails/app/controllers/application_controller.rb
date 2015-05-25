e8c31d771077d61166db0c947f5c6b6fc908ca11
Dan Asselin <dasselin86@gmail.com>
2015-05-25 18:36:32 -0400

    Refactor ApplicationController#parse_string_to_a method
    Add #downcase method to make sure all user-genereated genre data is in formatted uniformly

--- iradonrails/app/controllers/application_controller.rb
+++ iradonrails/app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def parse_string_to_a(string)
-    string.gsub(' ', '').split(',')
+    string.downcase.gsub(' ', '').split(',')
   end
 end
