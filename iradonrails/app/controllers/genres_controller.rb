9273556a958709ac059dc0e660d04e5d4c638dad
Dan Asselin <dasselin86@gmail.com>
2015-05-24 21:04:25 -0400

    Add code for Genres#destroy action

--- iradonrails/app/controllers/genres_controller.rb
+++ iradonrails/app/controllers/genres_controller.rb
@@ -35,6 +35,9 @@ class GenresController < ApplicationController
   end
 
   def destroy
+    genre = Genre.find(params[:id])
+    genre.destroy
+    redirect_to genres_path
   end
 
   def genre_params
