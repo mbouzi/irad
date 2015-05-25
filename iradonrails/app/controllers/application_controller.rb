32725cb621bc1b911d98b0328dd19e4616ef31c6
Dan Asselin <dasselin86@gmail.com>
2015-05-25 16:07:16 -0400

    Add #parse_string_to_a method to ApplicationController
    Method is used in artist_genres_controller#update action

--- iradonrails/app/controllers/application_controller.rb
+++ iradonrails/app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def parse_string_to_a(string)
+    string.gsub(' ', '').split(',')
+  end
 end
