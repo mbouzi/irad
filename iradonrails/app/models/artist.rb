0c914bf514d47745f49f14fbb697191a5f34bbe8
Dan Asselin <dasselin86@gmail.com>
2015-05-25 14:33:24 -0400

    Add Artist#all_artist_genres to artist model
    Instance method converts artist's genres collection to comma-separated string of same list to be rendered in text field of "edit artist genres" erb or elsewhere

--- iradonrails/app/models/artist.rb
+++ iradonrails/app/models/artist.rb
@@ -2,4 +2,13 @@ class Artist < ActiveRecord::Base
   belongs_to :record_label
   has_many :artist_genres
   has_many :genres, through: :artist_genres
+
+  def all_artist_genres
+    if self.genres.count < 1
+      'add genres'
+    else
+      self.genres.map{|genre|genre.name}.join(', ')
+    end
+  end
+
 end
