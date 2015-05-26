4e5c77981bbf2aa4baa4f5ffb4d4b28fdcf2e098
Dan Asselin <dasselin86@gmail.com>
2015-05-26 14:50:08 -0400

    Nest songs resource under artists in config/routes.rb

--- iradonrails/config/routes.rb
+++ iradonrails/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :artists do
     resource :artist_genres
     resources :albums
+    resources :songs
   end
 
   resources :record_labels, path: 'recordlabels'
