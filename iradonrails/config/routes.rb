668bfe77b93eb57ba8d79eb488c9db27f94bd9c5
Dan Asselin <dasselin86@gmail.com>
2015-05-25 20:31:23 -0400

    Add albums resources to config/routes.rb
    Nest albums resources under artists resources

--- iradonrails/config/routes.rb
+++ iradonrails/config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
 
   resources :artists do
     resource :artist_genres
+    resources :albums
   end
 
   resources :record_labels, path: 'recordlabels'
   resources :genres
+
 end
