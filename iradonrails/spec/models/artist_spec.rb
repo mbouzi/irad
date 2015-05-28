e216a9db05cdcb30474962ab2b0e0c28e6dac16e
Dan Asselin <dasselin86@gmail.com>
2015-05-28 10:34:01 -0400

    Psuedocode first artist model test

--- iradonrails/spec/models/artist_spec.rb
+++ iradonrails/spec/models/artist_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Artist, :type => :model do
-  it 'considers true to be true' do
-    expect(true).to eq(true)
+  it 'returns a comma-separate string of all the artist\'s genres' do
+    # stub artist
+    # stub genres
   end
 end
